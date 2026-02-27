e6e0d152ca3f4d23edfb58ef7b8ac8d6e93aca2c
GabrielFJanu <gabrielfreitasj2004@gmail.com>
2026-02-27 15:36:34 -0300

    new post submit button disabled

--- project4/network/static/network/index.js
+++ project4/network/static/network/index.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const newPostForm = document.querySelector("#new-post-form")
-    newPostForm.onsubmit = event => {
-        event.preventDefault();
-        event.target.querySelector("#new-post-content").value = '';
+    
+    if (newPostForm){ //usuário logado
+
+        const newPostContent = newPostForm.querySelector("#new-post-content")
+        const newPostSubmitButton = newPostForm.querySelector("#new-post-submit-button")
+
+        newPostSubmitButton.disabled = true; //inicia com submit button disabled
+
+        newPostForm.onsubmit = event => {
+            event.preventDefault(); //evita recarregar página
+            newPostContent.value = ''; //esvazia o content
+            newPostSubmitButton.disabled = true; //submit button volta disabled
+        };
+
+        newPostContent.oninput = () => {
+            newPostSubmitButton.disabled = newPostContent.value.trim() === ''; //se content.trim() vazio = submit button disabled
+        };
     }
 });
